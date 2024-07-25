7fa09bc8b763d5efa93635043ede3a47f9123f0c
quanvparadium-ubuntu <quan.vparoger@gmail.com>
2024-07-25 16:20:58 +0700

    docker: setup LAVIS successful

--- services/app/app.py
+++ services/app/app.py
@@ -1,7 +1,7 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-# from routes import mainRouter
+from routes import mainRouter
 from config.index import *
 from dotenv import load_dotenv
 load_dotenv()
@@ -16,7 +16,7 @@ origins = [
 # from connections.postgres import psg_manager
 # psg_manager.create_tables()
 
-# app.include_router(router=mainRouter, prefix='/api')
+app.include_router(router=mainRouter, prefix='/api')
 
 app.add_middleware(
     CORSMiddleware,
@@ -29,4 +29,6 @@ app.add_middleware(
 
 if __name__ == '__main__':   
     import uvicorn
+    print("HOST: ", os.getenv('HOST'))
+    print("PORT: ", os.getenv('PORT'))
     uvicorn.run(app, host=os.getenv('HOST'), port=int(os.getenv("PORT"))) 
