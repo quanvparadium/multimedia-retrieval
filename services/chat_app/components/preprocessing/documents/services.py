d47156e8d11bc16dc2228f971af6d413b0931f93
quanvparadium <quan.vparoger@gmail.com>
2024-08-30 00:47:22 +0700

    add document and index user

--- services/chat_app/components/preprocessing/documents/services.py
+++ services/chat_app/components/preprocessing/documents/services.py
@@ -11,8 +11,10 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from langchain_community.vectorstores import PGVector
+from langchain_community.retrievers import BM25Retriever
 # from langchain_postgres import PGVector
 from langchain.chains.retrieval_qa.base import RetrievalQA
+
 from langchain.retrievers.merger_retriever import MergerRetriever
 
 from langchain_core.documents import Document
@@ -21,7 +23,7 @@ from langchain_core.runnables import chain
 
 from config.constants import CONNECTION_STRING, GOOGLE_API_KEY
 from connections.postgres import psg_manager
-from entities.document import Document
+from entities.document import PGDocument
 
 import google.generativeai as genai
 genai.configure(api_key=GOOGLE_API_KEY)
@@ -30,7 +32,10 @@ from langchain_community.vectorstores.pgvector import DistanceStrategy
 
 import numpy as np
 
-LLMS_BOT = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY, temperature=0.2, convert_system_message_to_human=True),
+from sqlalchemy import text, sql, delete, exc
+
+
+# LLMS_BOT = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY, temperature=0.2, convert_system_message_to_human=True),
 
 
 def get_retriever_with_scores(vectorstore, query: str, k: int) -> List[Document]:
@@ -51,209 +56,221 @@ def get_output_document(document_file):
         "page": document_file['page'],
         "score": document_file["score"]
     }
-
-class DocumentPreprocessing:
+    
+class DocumentServicesV2:
     @staticmethod
-    def load_document(payload):
-        db = psg_manager.get_session()
-        result = db.query(Document).filter(Document.file_id == payload['file_id']).all()
-        if result:
-            return {
-                "message": "File existed"
-            }
-        if not os.path.exists(payload['file_path']):
-            return {
-                "message": "File path is not found"
-            }
-        pdf_loader = PyPDFLoader(payload['file_path'])
-        pages = pdf_loader.load_and_split()
-        # print("Page content: ", pages[0].page_content)
-        
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-        context = "\n\n".join(str(p.page_content) for p in pages)
-        texts = text_splitter.split_text(context)
-        print(len(texts))
-        
-        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=GOOGLE_API_KEY)
-        # db_retriever = PGVector.from_texts(
-        #     texts = texts,
-        #     embedding = embeddings,
-        #     collection_name = payload['file_id'],
-        #     distance_strategy = DistanceStrategy.COSINE,
-        #     connection_string = CONNECTION_STRING
-        # )
-        db_retriever = PGVector.from_documents(
-            documents=pages,
-            embedding = embeddings,
-            collection_name = payload['file_id'],
-            distance_strategy = DistanceStrategy.COSINE,
-            connection_string = CONNECTION_STRING
-        )
+    def _create_document(property):
+        db_session = psg_manager.get_session()
+        try:
+            document_data = PGDocument(
+                file_id = property['file_id'],
+                user_id = property['user_id'],
+                format = property['format'],
+                collection_id = property["collection_id"],
+                page_content = property["page_content"],
+                cmetadata = property["cmetadata"],
+                embedding = property["embedding"],
+                store = property['store'],
+                address= property['address'],
+            )
+            db_session.add(document_data)
+            db_session.commit()
+            db_session.refresh(document_data)
+            return True
+        except Exception as e:
+            print("Rollbacking ...")
+            db_session.rollback()
+            print("Error: ", e)
+            return False
+        finally:
+            db_session.close()          
+    
+    @staticmethod
+    def _semantic_search_by_file_id(file_id: str, query: str, limit: int =5):
+        embedding_function = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=GOOGLE_API_KEY)
+        query_embed = embedding_function.embed_query(query)
+        query_vector = f"[{','.join(map(str, query_embed))}]"
+        db_session = psg_manager.get_session()
 
-        db_document = Document(
-            file_id=payload['file_id'],
-            user_id=payload['user_id'],
-            format='pdf',
-            collection_id=payload['file_id'],
-            store='local',
-            address=payload['file_path'],
-        )
-        db.add(db_document)
-        db.commit()
-        db.refresh(db_document)
-        db.close()
-        print("Add successful: ", db_document.id)
-        
-        
-        return {
-            "message": "Add document successfully!"
+        sql_query = text("""
+            SELECT page_content, cmetadata, (embedding <=> :query_embeded) AS distance
+            FROM pg_documents
+            WHERE "file_id" = :file_id
+            ORDER BY distance
+            LIMIT :limit;
+        """)
+        params = {
+            "file_id": str(file_id),
+            "query_embeded": query_vector,  # Assuming req.query is the vector or will be converted to one
+            "limit": limit
         }
         
-        # embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=GOOGLE_API_KEY)
+        # # begin_time = datetime.datetime.now()
+        result = db_session.execute(sql_query, params).fetchall()
+        print("Semantic search result: ", result)
+        return result
+    
+    @staticmethod
+    def _semantic_search_by_user_id(user_id: str, query: str, limit: int = 5):
+        embedding_function = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=GOOGLE_API_KEY)
+        query_embed = embedding_function.embed_query(query)
+        query_vector = f"[{','.join(map(str, query_embed))}]"
+        db_session = psg_manager.get_session()
+
+        sql_query = text("""
+            SELECT page_content, cmetadata, (embedding <=> :query_embeded) AS distance
+            FROM pg_documents
+            WHERE "user_id" = :user_id
+            ORDER BY distance
+            LIMIT :limit;
+        """)
+        params = {
+            "user_id": str(user_id),
+            "query_embeded": query_vector,  # Assuming req.query is the vector or will be converted to one
+            "limit": limit
+        }
+        
+        # # begin_time = datetime.datetime.now()
+        result = db_session.execute(sql_query, params).fetchall()
+        print("Semantic search result: ", result)
+        return result
+    
+    
+    @staticmethod
+    def preprocessing(payload):
+        try: 
+            pdf_loader = PyPDFLoader(file_path= payload['file_path'])  
+            pages = pdf_loader.load_and_split()
+        except Exception as e:
+            print("Error: ", e)
+            return {
+                "message": "Please check file path again"
+            }
+        embedding_function = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=GOOGLE_API_KEY)
+        if pages is None:
+            return {
+                "message": "PDF file cannot be read or PDF file have no contents"
+            }
+        convert_texts = [d.page_content for d in pages]
+        metadatas = [d.metadata for d in pages]
+        if not metadatas:
+            metadatas = [{} for _ in convert_texts]
+        embeddings = embedding_function.embed_documents(list(convert_texts))
+        db_session = psg_manager.get_session()
+        try: 
+            user_id = payload['user_id']
+            file_id = payload['file_id']
+            format = payload['format'] 
+            file_path = payload['file_path']
+            store = payload['store']
+            
+            is_file_exists = db_session.query(sql.exists().where(PGDocument.file_id == file_id)).scalar()
+            if is_file_exists:
+                return {
+                    "message": "File is existed"
+                }
+            is_user_exists = db_session.query(sql.exists().where(PGDocument.user_id == user_id)).scalar() 
+            print("User id: ", is_user_exists)
+            for text, metadata, embedding in zip(convert_texts, metadatas, embeddings): 
+                property = {
+                    "file_id": file_id,
+                    "user_id": user_id,
+                    "format": format,
+                    "collection_id": f"collect_{file_id}",
+                    "page_content": text,
+                    "cmetadata": metadata,
+                    "embedding": embedding,
+                    "store": store,
+                    "address": file_path
+                }
+                is_created = DocumentServicesV2._create_document(property=property)
+                if is_created == False:
+                    print(f"Failed create file id {file_id} with metadata: {metadata}")
+
+            # Index user_id if not exists
+            if bool(is_user_exists) == False:
+                psg_manager.create_index_by_user(table_name= "pg_documents", user_id=user_id)
+            else:
+                print(f"User_id {user_id} indexed")
+                
+            return {
+                "message": "Document is preprocessed successfully!"
+            }
+        except Exception as e:
+            print("Error: ", e)
+            return {
+                "message": "Preprocessing failed!"
+            }
         
     @staticmethod
-    def search(payload):
-        db = psg_manager.get_session()
-        
-        # Truy vấn các tài liệu dựa trên fileId
-        documents = db.query(Document).filter(Document.file_id == payload['file_id']).all()
-        if not documents:
-            print(f"No documents found for fileId: {payload['file_id']}")
-            return []
-        
-        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=GOOGLE_API_KEY)
-        # db_vector = PGVector.from_texts(
-        #     texts=texts,
-        #     embedding=embeddings,
-        #     collection_name="your_collection_name",
-        #     distance_strategy=DistanceStrategy.COSINE,
-        #     connection_string=CONNECTION_STRING
-        # )
-        # retriever = db_vector.as_retriever(search_kwargs={"k": 3})
-        
-        db_vector = PGVector(
-            collection_name=payload['file_id'],
-            connection_string=CONNECTION_STRING,
-            embedding_function=embeddings
-        )
-
-        retriever = db_vector.as_retriever(search_kwargs={"k": payload['limit'], "include_metadata": True})
-        qa_chain = RetrievalQA.from_chain_type(
-            llm=ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY, temperature=0.2, convert_system_message_to_human=True),
-            retriever=retriever,
-            return_source_documents=True
-        )
-        
-        question = payload['query']
-        result = qa_chain.invoke({"query": question})
-        
-        # print(result['source_documents']) 
-        docs = result.get("source_documents", [])
-        print(docs[0])
-        for doc in result['source_documents']:
-            print("Id: ", (doc))
-        print(result.keys())
-        # print(result["result"])
-        # print("Source document: ", result['source_documents'])
-        # result = {
-        #     "result": "ok"
+    def search_all(payload):
+        """
+            Payload for search API:
+            - query: str
+            - limit: int
+            - user_id: str
+        """
+        db_session = psg_manager.get_session()
+        # is_user_exists = db_session.query(PGDocument).filter(PGDocument.user_id == payload['user_id']).exists()
+        is_user_exists = db_session.query(sql.exists().where(PGDocument.user_id == payload['user_id'])).scalar()
+        if is_user_exists == False:
+            return {
+                "message": "User_id is not existed."
+            }
+              
+        # print(is_user_exists)
+        # return {
+        #     "message": "testing.."
         # }
+        item_query = payload['query']
+        semantic_search_res = DocumentServicesV2._semantic_search_by_user_id(
+            user_id= payload['user_id'],
+            query= payload['query'],
+            limit= payload['limit']
+        )
+        # bm25_documents = [
+        #     {
+        #         'document': Document(page_content=page_content, metadata=cmetadata),
+        #         'scores': scores
+        #     } for page_content, cmetadata, scores in semantic_search_res
+        # ]
+        bm25_documents = [Document(page_content=page_content, metadata=cmetadata) for page_content, cmetadata, scores in semantic_search_res]
+        retriever = BM25Retriever.from_documents(documents= bm25_documents)
+        retriever.k = payload['limit']
+        print("Top limit: ", retriever.k)
+        result = retriever.invoke(input= item_query, config= {})
+        for rank, doc in enumerate(result):
+            print(f"Rank: {rank + 1} - Document: {doc.metadata}")
         return {
-            "answer": result["result"],
-            "source": result['source_documents'][0].metadata['source'],
-            "page": result['source_documents'][0].metadata['page'],
+            "message": "Search successfully!"
         }
     
-    def search_folder(payload, generate=False):
-        db = psg_manager.get_session()
-        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=GOOGLE_API_KEY)
-        query_embed = embeddings.embed_query(payload['query'])
-        result = None
-        # Truy vấn các tài liệu dựa trên fileId
-        files = payload['files']
-        similarity_scores = []
-        for file_id in files:
-            
-            documents = db.query(Document).filter(Document.file_id == file_id).all()
-            if not documents:
-                print(f"No documents found for fileId: {payload['file_id']}")
-                # return []
-                
-            db_vector = PGVector(
-                collection_name=file_id,
-                connection_string=CONNECTION_STRING,
-                embedding_function=embeddings
-            )     
-            print("=" * 10, " BEGIN SEARCH ", "="*10) 
-            
-            begin_time = datetime.now()     
-            db_vector.asimilarity_search_with_score  
-            results = db_vector.similarity_search_with_score_by_vector(embedding=query_embed, k= payload['limit'])
-            print("Total search time: ", datetime.now() - begin_time)
-            for result in results:
-                current_metadata = result[0].metadata
-                # print("Score: ", result[1])
-                # print("Source: ", result[0].page_content)
-                print("Score: ", result[1])
-                print(current_metadata)
-                print("="*10)
-                similarity_scores.append({
-                    **current_metadata,
-                    "collection_name": file_id,
-                    "score": result[1]
-                })
-            # print(result_search[])
-            print("=" * 10, " END SEARCH ", "="*10)         
-            
-            # retriever = db_vector.as_retriever(
-            #     search_type="similarity", 
-            #     search_kwargs={
-            #         "k": 5, 
-            #         "include_metadata": True
-            #     }
-            # )
-            # retrievers.append(retriever)
-        def return_score(obj):
-            return obj['score']
-        begin_time = datetime.now()       
-        sort_sim_scores = sorted(similarity_scores, key=return_score)
-        print("Length: ", len(sort_sim_scores))
+    @staticmethod
+    def search_many_files(payload):
+        """
+            Payload for search API:
+            - query
+            - limit
+            - files
+        """
+    
+    @staticmethod
+    def delete_file(file_id, user_id):
+        db_session = psg_manager.get_session()
+        rows_to_delete = db_session.query(PGDocument).filter_by(file_id= file_id).all()
+        print(len(rows_to_delete))
+        for row in rows_to_delete:
+            db_session.delete(row)  
+            db_session.commit()  
         
-        top1 = sort_sim_scores[0]
-        print("Total sort time: ", datetime.now() - begin_time)
-      
-        
-        collection_name = top1['collection_name']
-        print("Collection name: ", collection_name)
-        if generate:  
-            top1_vectorstore = PGVector(
-                collection_name=collection_name,
-                connection_string=CONNECTION_STRING,
-                embedding_function=embeddings
-            )  
-            retriever = top1_vectorstore.as_retriever(
-                search_kwargs={
-                    "k": payload['limit'], 
-                    "include_metadata": True
-                }
-            )
-            qa_chain = RetrievalQA.from_chain_type(
-                llm=ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY, temperature=0.2, convert_system_message_to_human=True),
-                retriever=retriever,
-                return_source_documents=True
-            )
-            
-            question = payload['query']
-            result = qa_chain.invoke({"query": question})
-            for document in result["source_documents"]:
-                print("Metadata: ", document.metadata)
-                print("="*30)
-            print(result["result"])
-        
+        # db_session.commit()    
+        # delete_q = PGDocument.__table__.delete().where(Report.data == 'test')
+        # print(result)
+        # files = db_session.query
         return {
-            "message": "Search document by text successfully!",
-            "result": [get_output_document(doc_file) for doc_file in sort_sim_scores]
+            "message": "testing delete..."
         }
         
-            
+
+
             
--- services/chat_app/entities/document.py
+++ services/chat_app/entities/document.py
@@ -1,14 +1,17 @@
 import os
+import numpy as np
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from connections.postgres import psg_manager
 from sqlalchemy import Column, Integer, String, TIMESTAMP, func
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.hybrid import hybrid_method
 from pgvector.sqlalchemy import Vector
 
 
-class Document(psg_manager.Base):
-    __tablename__ = 'documents'
+class PGDocument(psg_manager.Base):
+    __tablename__ = 'pg_documents'
 
     id = Column(Integer, autoincrement=True, primary_key=True)
     file_id = Column(String, nullable=False)
@@ -16,9 +19,35 @@ class Document(psg_manager.Base):
     format = Column(String, nullable=False)
     
     collection_id = Column(String, nullable=True)
+    page_content = Column(String, nullable=True)
+    cmetadata = Column(JSONB, nullable=True)
+    embedding = Column(Vector(768), nullable=True)
     store = Column(String, nullable=False, default='local')
     address = Column(String, nullable=False)
     createdAt = Column(TIMESTAMP, server_default=func.now(), nullable=False)
     updatedAt = Column(TIMESTAMP, server_default=func.now(), onupdate=func.now(), nullable=False)
 
+    @hybrid_method
+    def cosine_distance(self, query_embedding):
+        # Chuyển đổi embedding và query_embedding thành numpy arrays
+        db_embedding = np.array(self.embedding)
+        query_embedding = np.array(query_embedding)
+
+        # Tính toán tích vô hướng (dot product) giữa hai vectors
+        dot_product = np.dot(db_embedding, query_embedding)
+
+        # Tính toán độ lớn (norm) của từng vector
+        norm_db_embedding = np.linalg.norm(db_embedding)
+        norm_query_embedding = np.linalg.norm(query_embedding)
+
+        # Tính toán độ tương đồng cosine
+        cosine_similarity = dot_product / (norm_db_embedding * norm_query_embedding)
+
+        # Khoảng cách cosine là 1 trừ đi độ tương đồng cosine
+        cosine_distance = 1 - cosine_similarity
+
+        return cosine_distance
     
+    @hybrid_method
+    def l2_distance(self, query_embedding):
+        return (func.sum((self.embedding - query_embedding) * (self.embedding - query_embedding)))
